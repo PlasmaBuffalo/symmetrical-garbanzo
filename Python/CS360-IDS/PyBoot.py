c401b8832aedf19d8a8ee990d93c5000596309b5
PlasmaBuffalo <89225375+PlasmaBuffalo@users.noreply.github.com>
2023-08-31 14:03:46 -0400

    add project files

---
+++ Python/CS360-IDS/PyBoot.py
@@ -0,0 +1,11 @@
+#Python Boot camp
+
+#normal stuff above this
+#now class grabs
+#f = open("nameOfFile.txt")
+#print(f.read())
+
+#with open("example.txt") as file
+#     line = [line.rstrip() for line in file]
+#     print(line)
+
