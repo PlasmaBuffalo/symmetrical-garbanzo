db4cdfcc1147ba8fc60a79adaeedc4d22954591c
PlasmaBuffalo <89225375+PlasmaBuffalo@users.noreply.github.com>
2023-09-01 18:05:07 -0400

    it works now

--- CS360-IDS/ReadHTML.py
+++ CS360-IDS/ReadHTML.py
@@ -13,23 +13,35 @@
 
 openTitleTag = "<title>"
 closeTitleTag = "</title>"
-hrefLinkPattern = 'href="..."'
+linkPattern = 'href='
 linkCount = 0
 pageTitle = ""
 
-with open("Python/CS360-IDS/ClassWebsite.html") as file:
+with open("CS360-IDS/ClassWebsite.html") as file:
     line = [line.rstrip() for line in file]
     for i in range (0, len(line)):
         #step: find <title>Data Science</title>
         titleIndex = line[i].find(openTitleTag)
         endIndex = line[i].find(closeTitleTag)
-        #step: find each href tag and ensure its link does not begin with '#'
-        #pattern is href="..."
-        hrefIndex = line[i].find(openTitleTag)
+
         #step: use substring to strip tags from text
+        # since the find function returns -1 if not found, this will detect when indices are different
         if (titleIndex != endIndex):
             pageTitle = (line[i][titleIndex+len(openTitleTag):endIndex])
 
+        #step: find each href tag and ensure its link does not begin with '#'
+        #make sure we get every href tag for this line, so we keep track of last href tag index
+        lastLinkIndex = 0
+        for j in range(0, line[i].count(linkPattern)):
+            hrefIndex = line[i].find(linkPattern, lastLinkIndex)
+            if (hrefIndex != -1):
+                lastLinkIndex = hrefIndex+len(linkPattern)
+                firstLinkChar = line[i][hrefIndex+len(linkPattern)+1]
+                if (firstLinkChar != "#"):
+                    linkCount += 1
 
 
-print(pageTitle)
+
+
+print("Page Title:",pageTitle)
+print("Link Count:",linkCount)
