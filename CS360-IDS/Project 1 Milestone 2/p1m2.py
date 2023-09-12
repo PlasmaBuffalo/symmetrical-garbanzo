9fe56cafc18dea9300a4d6a6a8671cfda52019b4
PlasmaBuffalo <89225375+PlasmaBuffalo@users.noreply.github.com>
2023-09-12 10:57:15 -0400

    deleted:    CS130/Project 1 LZ/README.md#	new file:   CS360-IDS/Project 1 Milestone 2/p1m2.py

---
+++ CS360-IDS/Project 1 Milestone 2/p1m2.py
@@ -0,0 +1,17 @@
+"""
+The goal of this milestone is for you to scrape data from an actual website and start to analyze the results. The project should be completed in individually in python without the assistance of any artificial intelligences.
+
+Web Scraper - Take all of the text from the top 20 articles coming from one of the following web news sources:
+
+espn.com
+cnn.com
+goodblacknews.org
+huffingtonpost.com
+ign.com
+theonion.com
+
+Statistics - Print a list of the titles of the web pages you are pulling text from and then print the mean and median number of words for all 20 articles.
+
+Visualize - Create a Word Cloud using your list of most common words that shows the top 50 (or up to 200) words and a bar chart to show the relative frequencies of the top 15 most frequent words. (Note: these should be words that the average viewer of the website would see, not code from the html)
+
+"""
