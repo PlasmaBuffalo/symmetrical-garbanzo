543808bc27628f358aaa0fd07322e13a7fb92f88
PlasmaBuffalo <89225375+PlasmaBuffalo@users.noreply.github.com>
2023-09-19 10:07:06 -0400

    idk
    more sort

--- SMP/WebScrape.py
+++ SMP/WebScrape.py
@@ -2,22 +2,20 @@
 # needs to create a JSON with all classes required for the major provided
 
 # this is the page containing all smcm majors and minors
-base_url = "https://www.smcm.edu/academics/majors-minors-concentrations/"
-
-#http://catalog.smcm.edu/
-#this link is specifically a hardcoded link to the CS Major requirements page
-specific = "https://www.smcm.edu/mathcs/academic-offerings/what-you-learn-cs/requirements/#option1"
+base_url = "http://catalog.smcm.edu/"
 
 from bs4 import BeautifulSoup as bs
 import requests
 
 
-response = requests.get(specific)
+response = requests.get(base_url)
 text = response.content
 
 soup_page = bs(text, "html.parser")
 
+"""
 print(soup_page.find_all(class_="entry catalog"))
 #h1 = "Degree requirements for x major"
 
 textBlocks = soup_page.article.get_text(strip=True, separator='\n').splitlines() # type:ignore
+ """
