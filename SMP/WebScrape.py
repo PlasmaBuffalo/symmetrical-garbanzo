8534fb14b5a0518d605cf37bb76c2b65223c0898
PlasmaBuffalo <89225375+PlasmaBuffalo@users.noreply.github.com>
2023-09-28 11:10:36 -0400

    fix the link getter

--- SMP/WebScrape.py
+++ SMP/WebScrape.py
@@ -1,17 +1,30 @@
 #Schedule maker web scraper version 1: hardcoded to the right website
 # needs to create a JSON with all classes required for the major provided
 
-# this is the page containing all smcm majors and minors
-base_url = "http://catalog.smcm.edu/"
 
 from bs4 import BeautifulSoup as bs
 import requests
 
 
+# this is the page containing all smcm majors and minors
+# base_url = "http://catalog.smcm.edu/"
+base_url = "https://catalog.smcm.edu/content.php?catoid=2&navoid=47"
+
+# get page response
 response = requests.get(base_url)
 text = response.content
 
+tags = ["Major", "Minor", "BA", "BS"]
+links = []
+
 soup_page = bs(text, "html.parser")
+for link in soup_page.find_all('a'):
+    for kwd in tags:
+        if link.text.find(kwd) != -1:
+            links.append(link)
+
+for item in links:
+    print(item)
 
 """
 print(soup_page.find_all(class_="entry catalog"))
