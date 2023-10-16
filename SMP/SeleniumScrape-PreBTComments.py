0d9d02933b077269cb50afb21e076faa327dd854
PlasmaBuffalo <89225375+PlasmaBuffalo@users.noreply.github.com>
2023-10-16 16:16:17 -0400

    dropdowns work now

--- SMP/SeleniumScrape-PreBTComments.py
+++ SMP/SeleniumScrape-PreBTComments.py
@@ -8,12 +8,10 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 # start by defining the options
 options = webdriver.ChromeOptions()
-# we run this headless normally, but this is commented out for debugging purposes
 # options.add_argument('--headless')
-
-# certificate errors should be resolved now, leaving here just in case
-# options.add_argument('--ignore-certificate-errors')
-# options.add_argument('--ignore-ssl-errors')
+# should be resolved now, can remove
+options.add_argument('--ignore-certificate-errors')
+options.add_argument('--ignore-ssl-errors')
 
 # we don't need it as the page also populated with the running javascript code.
 options.page_load_strategy = 'none'
@@ -45,27 +43,31 @@ time.sleep(2)
 # now we're on the page needed to gather all course info after many clicks
 # now we need to click on each course's dropdown to reveal all the information we need to scrape and store
 
-# get all links with the aria-expanded attribute set to false
+# these selectors DO NOT WORK to open the class link expandable
+# driver.find_element(By.CSS_SELECTOR, "[aria-expanded='false']").click()
+# driver.find_element(By.XPATH, "a[href='#/usr/local/webroot/acalog-legacy/shared/htdocs_gateway/ajax/preview_course.php']")
+
+# this prints out the name of every class we see, but doesn't work for 
+"""
+clickables = driver.find_elements(By.CSS_SELECTOR, "[aria-expanded='false']")
+for item in clickables:
+    #focus the driver on the item
+    print(item.text)
+ """
+
+# Get all links with the aria-expanded attribute set to false
 links = driver.find_elements(By.CSS_SELECTOR, "[aria-expanded='false']")
 
-# iterate over the links and click on them if they are visible
+# Iterate over the links and click on them if they are visible
 for link in links:
     if link.is_displayed():
         link.click()
 
-# now that everything is dropped down, we can scrape all data from the site
+
+# this clicks on the first required class
+# driver.find_element(By.XPATH, "/html[1]/body[1]/table[1]/tbody[1]/tr[3]/td[2]/table[1]/tbody[1]/tr[2]/td[2]/table[1]/tbody[1]/tr[1]/td[1]/table[1]/tbody[1]/tr[2]/td[1]/div[1]/div[2]/div[2]/ul[1]/li[1]/span[1]/a[1]").click()
+# this clicks on the second required class
+# driver.find_element(By.XPATH, "/html[1]/body[1]/table[1]/tbody[1]/tr[3]/td[2]/table[1]/tbody[1]/tr[2]/td[2]/table[1]/tbody[1]/tr[1]/td[1]/table[1]/tbody[1]/tr[2]/td[1]/div[1]/div[2]/div[2]/ul[1]/li[2]/span[1]/a[1]").click()
 
 
-# Get the HTML of the current page
-html = driver.page_source
-
-# Print the HTML to the console (Debug)
-# print(html)
-
-# this writes the current HTML as a new document we can now work with and parse
-with open("page.html", "w") as f:
-    f.write(html)
-
-
-
-time.sleep(30)
+time.sleep(3)
