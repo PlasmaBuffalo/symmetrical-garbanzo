b697f07adb9602109aedac108120bd8440b7a3a0
PlasmaBuffalo <89225375+PlasmaBuffalo@users.noreply.github.com>
2024-04-26 02:27:06 -0400

    fat progress

--- SMP/SeleniumScrape.py
+++ SMP/SeleniumScrape.py
@@ -7,6 +7,13 @@ from selenium.webdriver import Chrome
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
+from kivy.app import App
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
+from kivy.uix.button import Button
+from kivy.uix.widget import Widget
+from kivy.uix.behaviors import DragBehavior
 
 force_scrape = False
 
@@ -150,73 +157,210 @@ with open("SMP/classText.txt", "w", encoding="utf-8") as f:
         f.write(bigString)
     f.close()
 
-# next step: create a dict per line organizing course information
+# Define the Course class to store information about each course as we read it in
+class Course:
+    def __init__(self, department, number, name, credit_hours, frequency, satisfies, prerequisites, description, category):
+        self.department = department
+        self.number = number
+        self.name = name
+        self.credit_hours = credit_hours
+        self.frequency = frequency
+        self.satisfies = satisfies
+        self.prerequisites = prerequisites
+        self.description = description
+        self.category = category
 
-course_list = []
+# to store all the Course objects we create below
+course_object_list = []
 
-# for each line in classText.txt, check if line is valid and create a dict for it
-
+# for each line in classText.txt, check if line is valid and create a Course object for it
 for line in open("SMP/classText.txt", "r", encoding="utf-8"):
     # if the line is valid (contains a course code) which we have accounted for
     if (line[0:8] in code_list):
-        # create a dict for the course
-        course_dict = {}
+        # create a Course object for the course
+        course = Course(line[0:4], line[5:8], "", "", "", "", "", "", "")
 
-        # add the course code to the dict, first 8 characters
-        course_dict["code"] = line[0:8]
-        # then remove the course code from the line
+        # remove the course code from the line
         line = line[8:]
 
-        # add the course name to the dict, from end of code to "Credit"
-        course_dict["name"] = line[:line.find("Credit") - 1]
+        # add the course name to the Course object, from end of code to "Credit"
+        course.name = line[:line.find("Credit") - 1]
         # then remove the course name from the line
         line = line[line.find("Credit") - 1:]
 
-        # add the course credits to the dict, from beginning of "Credit" to "Frequency"
-        course_dict["credit_hours"] = line[line.find("Credit"):line.find("Frequency")]
+        # add the course credits to the Course object, from beginning of "Credit" to "Frequency"
+        course.credit_hours = line[line.find("Credit"):line.find("Frequency")]
         # then remove the course credits from the line
         line = line[line.find("Frequency"):]
 
-        # add the course frequency to the dict, from beginning of "Frequency" to end of "semester"
-        course_dict["frequency"] = line[line.find("Frequency"):line.find("semester")+8]
+        # add the course frequency to the Course object, from beginning of "Frequency" to end of "semester"
+        course.frequency = line[line.find("Frequency"):line.find("semester")+8]
         # then remove the course frequency from the line
         line = line[line.find(":"):]
 
-        # if the course satisfies a requirement, we'll add that to the dict
+        # if the course satisfies a requirement, add that to the Course object
         if line.find("Course Satisfies") != -1:
-            course_dict["satisfies"] = line[line.find("Course Satisfies")+17:line.find(".")]
+            course.satisfies = line[line.find("Course Satisfies")+17:line.find(".")]
         # null otherwise
         else:
-            course_dict["satisfies"] = ""
+            course.satisfies = ""
         # then remove the satisfies from the line
         line = line[line.find(".")+1:]
 
-        # if prerequisites exist, we'll add that to the dict
+        # if prerequisites exist, add that to the Course object
         if line.find("Prerequisite(s):") != -1:
             prereq_start = line.find("Prerequisite(s):")
-            # then we find the end of the prereqs by looking for the next period
+            # then find the end of the prereqs by looking for the next period
             prereq_end = line.find(".", prereq_start)
-            # then we add the prereqs to the dict after some filtering
-            course_dict["prerequisites"] = line[prereq_start:prereq_end].replace("Prerequisite(s):", "").replace("or permission of the instructor", "")
+            # then add the prereqs to the Course object after some filtering
+            course.prerequisites = line[prereq_start:prereq_end].replace("Prerequisite(s):", "").replace("or permission of the instructor", "")
             # then remove the prereqs from the line
             line = line[:prereq_start]
-            # null otherwise
+        # null otherwise
         else:
-            course_dict["prerequisites"] = ""
+            course.prerequisites = ""
         # then remove the prereqs from the line
+        # line = line[line.find(".")+1:]
 
-        # add the course description to the dict; it should be the only thing left
-        course_dict["description"] = line
-        # add the course dict to the course list
-        course_list.append(course_dict)
+        # add the course description to the Course object; it should be the only thing left
+        course.description = line
+        # last, assign the course category to be either required or elective depending on the course code
+        # if the numerical component is <299, it is a required course. if >= 490, it is a final requirement. Else, it is an elective course.
+        #! Note that this is not a perfect system: these rules do not apply to other departments
+        if int(course.number) < 299:
+            course.category = "Required"
+        elif int(course.number) >= 490:
+            course.category = "Capstone"
+        else:
+            course.category = "Elective"
+        # add the Course object to the course_object_list
+        course_object_list.append(course)
 
-# print the course list to the console
-# for course in course_list:
+# print the course_object_list to the console
+# for course in course_object_list:
 #     print(course)
 #     print("\n")
+
 
 # we need to know if the course is a required or elective course
 # - all required courses need to be in the final calendar
 # - a certain number of elective courses need to be in the final calendar
 # - a capstone course needs to be in the final calendar
 # - a certain number of credits need to be in the final calendar
+class DraggableCourse(Button, DragBehavior):
+    def __init__(self, course, **kwargs):
+        super(DraggableCourse, self).__init__(**kwargs)
+        self.course = course
+        self.semester = None
+        self.year = None
+        # self.size_hint = (100, 100) (currently breaks drag functionality)
+        self.size = self.texture_size
+        self.background_color = (1, 0, 0, 1)
+        # make the label text appear in the center of the button
+        self.halign = 'center'
+        self.valign = 'center'
+
+    def on_touch_down(self, touch):
+        if self.collide_point(*touch.pos):
+            self.drag_start(touch)
+            # then reset the semester and year attributes to None
+            self.semester = None
+            self.year = None
+            self.update_course_status()
+            return True
+        return super(DraggableCourse, self).on_touch_down(touch)
+
+    def drag_update(self, touch):
+        if self._drag_touch is touch:
+            self.pos = (touch.x - self.width / 2, touch.y - self.height / 2)
+
+    def drag_start(self, touch):
+        # Add code to handle the drag start event
+        self._drag_touch = touch
+        self._drag_offset = self.pos[0] - touch.x, self.pos[1] - touch.y
+        self.opacity = 0.5
+
+    def on_touch_move(self, touch):
+        if self._drag_touch is touch:
+            self.drag_update(touch)
+
+    def update_course_status(self):
+            # Add code to update the schedule status
+            if self.year is not None and self.semester is not None:
+                self.background_color = (0, 1, 0, 1)  # Change color to green
+            else:
+                self.background_color = (1, 0, 0, 1)  # Change color to red
+
+    def on_touch_up(self, touch):
+        if self._drag_touch is touch:
+            for widget in self.parent.parent.parent.children[0].children: # This is a hacky way to get the calendar_layout
+                if isinstance(widget, Label) and self.collide_widget(widget):
+                    print(f"Collided with {widget.text}")
+                    label_text = widget.text.split(" - ")
+                    self.semester = label_text[0]
+                    self.year = label_text[1]
+                    self.update_course_status()
+
+            self.opacity = 1
+            self._drag_touch = None
+            return True
+        return super(DraggableCourse, self).on_touch_up(touch)
+
+class CoursePlannerApp(App):
+    def build(self):
+
+        root = BoxLayout(padding=25)
+
+        # Create a new GridLayout with 3 columns
+        side_list_layout = GridLayout(cols=3, spacing=(10, 10))
+
+        ## Add widgets to the top layout:
+
+        # Add labels for each column
+        label1 = Label(text="Required")
+        label2 = Label(text="Elective")
+        label3 = Label(text="Capstone")
+
+        # Create box layouts for each column
+        column1_layout = BoxLayout(orientation='vertical')
+        column2_layout = BoxLayout(orientation='vertical')
+        column3_layout = BoxLayout(orientation='vertical')
+
+        # Add labels to the box layouts
+        column1_layout.add_widget(label1)
+        column2_layout.add_widget(label2)
+        column3_layout.add_widget(label3)
+
+        # Add draggable buttons for each course in the all_courses list
+        for course in course_object_list:
+            draggable_course = DraggableCourse(course, text=course.department + ' ' + course.number + '\n' + course.name)
+            if course.category == "Required":
+                column1_layout.add_widget(draggable_course)
+            elif course.category == "Elective":
+                column2_layout.add_widget(draggable_course)
+            elif course.category == "Capstone":
+                column3_layout.add_widget(draggable_course)
+
+        # Add the box layouts to the top layout
+        side_list_layout.add_widget(column1_layout)
+        side_list_layout.add_widget(column2_layout)
+        side_list_layout.add_widget(column3_layout)
+
+        # Add the top layout to the root layout
+        root.add_widget(side_list_layout)
+
+        # Create a GridLayout to display the calendar
+        calendar_layout = GridLayout(cols=4, spacing=(40, 40))
+
+        # Add labels for each semester and year
+        for semester in ['Fall', 'Spring']:
+            for year in ['Year 1', 'Year 2', 'Year 3', 'Year 4']:
+                label = Label(text=f"{semester} - {year}")
+                calendar_layout.add_widget(label)
+                # column1_layout.add_widget(draggable_course)
+
+        root.add_widget(calendar_layout)
+        return root
+
+if __name__ == '__main__':
+    CoursePlannerApp().run()
